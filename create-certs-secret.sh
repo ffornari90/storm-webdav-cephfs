f7950883a8fccc36d71dafbe44d90bf9f1703ae7
Federico Fornari <federico.fornari@cnaf.infn.it>
2022-12-17 17:30:28 +0100

    Modified namespace name

--- create-certs-secret.sh
+++ create-certs-secret.sh
@@ -1,5 +1,15 @@
-mkdir -p certs
-openssl req -x509 -newkey rsa:4096 -days 365 -nodes -sha256 -keyout certs/private.key -out certs/public.crt -subj "/CN=storm-webdav-storm-webdav-ns.apps.cnsa.cr.cnaf.infn.it" \
--addext "subjectAltName = DNS:storm-webdav-storm-webdav-ns.apps.cnsa.cr.cnaf.infn.it"
-cp certs/public.crt certs/ca.crt
-kubectl create secret generic tls-ssl-storm-webdav -n storm-webdav-ns --from-file=./certs/private.key --from-file=./certs/public.crt --from-file=./certs/ca.crt
+if [ ! -d "certs" ]
+then
+  mkdir -p certs
+  openssl req -x509 -newkey rsa:4096 -days 365 \
+  -nodes -sha256 -keyout certs/private.key -out certs/public.crt \
+  -subj "/CN=storm-webdav-webdav-ns.apps.cnsa.cr.cnaf.infn.it" \
+  -addext "subjectAltName = DNS:storm-webdav-webdav-ns.apps.cnsa.cr.cnaf.infn.it"
+  cp certs/public.crt certs/ca.crt
+fi
+kubectl create secret generic \
+ tls-ssl-storm-webdav \
+ -n webdav-ns \
+ --from-file=./certs/private.key \
+ --from-file=./certs/public.crt \
+ --from-file=./certs/ca.crt
--- create-storm-webdav-route.sh
+++ create-storm-webdav-route.sh
@@ -1,1 +1,1 @@
-oc create route passthrough --namespace storm-webdav-ns --service=storm-webdav --port=storm-webdav-https
+oc create route passthrough --namespace webdav-ns --service=storm-webdav --port=storm-webdav-https
